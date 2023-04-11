3a41bd4d4b38bc670ca468d94e23d71731f5eadf
FullStackCorm <corinne.m.dev@gmail.com>
2023-04-11 15:03:12 -0400

    changed med items to med cards

--- client/src/components/cards/MedCard.jsx
+++ client/src/components/cards/MedCard.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { deleteMedication } from '../../features/medications/medSlice';
 import { useDispatch } from 'react-redux';
+import {deleteMedication} from '../../features/medications/medSlice';
+import { Container, Grid, Box, Button } from '@mui/material';
 
-const MedCard = (props) => {
-  const medication = props.medication;
+// https://mui.com/material-ui/react-dialog/ to confirm delete medication
+
+function MedCard({medication}) {
+    const dispatch = useDispatch()
 
   return (
-    <div className='card-container'>
-      <div className='desc card-item py-5 px-3.5 text-center'>
-        <h2>
-          <Link to={`/medications/${medication._id}`}>{`${medication.name} ${medication.strength}`}</Link>
-        </h2>
+    <div>
+      <Grid container margin='auto' width='80%'> 
+        <Grid item xs={12}>
+          <h5>Medication: {medication.name} {medication.strength}{medication.doseForm}</h5>
+        </Grid>
+        <Grid item xs={12}>
+          <span>Directions: {medication.directions}</span>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <span>Time of Day: {medication.timeOfDay}</span>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <span>Prescriber: {medication.prescriber}</span>
+        </Grid>
 
-        <p>{medication.directions}</p>
-      </div>
-    </div>
+        <Grid item xs={6}>
+          <Button 
+            type='button' 
+          >
+            Edit Medication
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button 
+            type='button'
+            onClick={() => dispatch(deleteMedication(medication._id))} 
+          >
+            Delete Medication
+          </Button>
+        </Grid>   
+      </Grid> 
+      <hr />
+    </div>  
   );
-};
+}
 
-export default MedCard;
+export default MedCard
