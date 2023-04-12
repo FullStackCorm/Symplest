4ac9437762ab07809330f1416f6c03b5a65e2826
FullStackCorm <corinne.m.dev@gmail.com>
2023-04-12 12:51:45 -0400

    converted to note card, but committing this so i can delete it locally

--- client/src/components/items/NoteItem.jsx
+++ client/src/components/items/NoteItem.jsx
@@ -1,18 +1,33 @@
 import { useDispatch } from 'react-redux';
-import {deleteNote} from '../../features/notes/noteSlice';
+import { deleteNote } from '../../features/notes/noteSlice';
+import { Card, Grid, Button } from '@mui/material';
 
 function NoteItem({note}) {
     const dispatch = useDispatch()
 
   return (
     <div className="note">
-        <div>
-            {new Date(note.createdAt).toLocaleString('en-US')}
-        </div>
-        <h2>{note.text}</h2>
-        <button onClick={() => dispatch(deleteNote(note._id))} className="close">X</button>
+      <Card container
+        sx={{
+          variant: 'outlined',
+          margin: 'auto',
+          width: '100%',
+          marginBottom: '2rem',
+          textAlign:'center',
+        }}>
+        <Grid item xs={10} paddingBottom='1rem'>
+          <h5>{note.text}</h5>
+        </Grid>
+        <Grid item xs={2} paddingTop='1rem'>
+            <button onClick={() => dispatch(deleteNote(note._id))} className="close" style={{color: 'white'}}>X</button>
+        </Grid>
+        <Grid item xs={10}>
+          {new Date(note.createdAt).toLocaleString('en-US')}
+        </Grid>
+      </Card>
+        
     </div>
-  )
+  );
 }
 
 export default NoteItem
