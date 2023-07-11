2d7a074a4db792218e0f8036657a1ab9a91eda38
FullStackCorm <corinne.m.dev@gmail.com>
2023-07-11 07:57:54 -0400

    .

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("/", (req, res) =>
     res.sendFile(
-      path.join(__dirname, "build ", "index.html")
+      path.join(__dirname, "build", "index.html")
     )
   );
 } else {
