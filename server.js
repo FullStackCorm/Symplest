bf32b8f2384d2636ad5bfe95de9c7a571d1c99e0
FullStackCorm <corinne.m.dev@gmail.com>
2023-07-10 20:45:11 -0400

    deploying to cyclic

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ app.use('/api/symptoms', require('./routes/symptomRoutes'));
 app.use('/api/moods', require('./routes/moodRoutes'));
 app.use('/api/pain', require('./routes/painRoutes'));
 
-// Serve frontend
+// Serve client
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      path.resolve(__dirname, "../", "client", "build", "index.html")
     )
   );
 } else {
